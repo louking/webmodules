43c327496124a2cb4085db5b06e8e0519bb58200
Lou King <lking@pobox.com>
2023-05-11 15:48:43 -0400

    nav tested, admin landing (missing files) (#29, #37, #36)

---
+++ app/src/webmodules/views/admin/__init__.py
@@ -0,0 +1,11 @@
+'''
+blueprint for this folder
+'''
+
+from flask import Blueprint
+
+# create blueprint first
+bp = Blueprint('admin', __name__.split('.')[0], url_prefix='/admin', static_folder='static/admin', template_folder='templates/admin')
+
+# specific views
+from . import home
---
+++ app/src/webmodules/views/admin/home.py
@@ -0,0 +1,30 @@
+'''
+home - public views
+=================================
+'''
+
+# pypi
+from flask import render_template, jsonify
+from flask.views import MethodView
+from loutilities.flask_helpers.mailer import sendmail
+from flask_security import auth_required
+
+# homegrown
+from . import bp
+from ...model import Blog
+
+class AdminHome(MethodView):
+    decorators = [auth_required()]
+
+    def get(self):
+        return render_template('home.jinja2',
+                               pagename='Admin Home',
+                               # causes redirect to current interest if bare url used
+                               url_rule='/admin',
+                            #    url_rule='/admin/<interest>',
+                               )
+
+admin_view = AdminHome.as_view('home')
+bp.add_url_rule('/', view_func=admin_view, methods=['GET',])
+# bp.add_url_rule('/<interest>', view_func=admin_view, methods=['GET',])
+
